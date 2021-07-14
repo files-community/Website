436747ce4ccc4a76a64a08726823a0e175de0d4d
Yair Aichenbaum <39923744+yaichenbaum@users.noreply.github.com>
2021-07-14 12:40:31 -0400

    Hero image (#1)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -56,33 +56,53 @@
 
 <PageSection id="hero-section">
     <canvas width="32" height="32" bind:this={canvas}/>
-    <h1>Files</h1>
-    <p>A modern file explorer that pushes the boundaries of the platform.</p>
-    <div class="hero-buttons">
-        <Button
-            href={windows ? `ms-windows-store://pdp/?ProductId=${links.storeId}` : `https://www.microsoft.com/en-us/p/files/${links.storeId}`}
-            target="_blank"
-            rel="noreferrer noopener"
-            style="accent"
-        >
-            {@html ArrowDownload}
-            <div class="hero-button-inner">
-                <h5>Download {version || ""}</h5>
-                <span>Get it from Microsoft</span>
-            </div>
-        </Button>
-        <Button
-            href="https://github.com/{links.github.owner}/{links.github.repo}/"
-            target="_blank"
-            rel="noreferrer noopener"
-        >
-            {@html Code}
-            <div class="hero-button-inner">
-                <h5>View GitHub</h5>
-                <span>Files is open source!</span>
-            </div>
-        </Button>
+    <div class="hero-left">
+        <h1>Files</h1>
+        <p>A modern file explorer that pushes the boundaries of the platform.</p>
+        <div>
+            <Button
+                href={windows ? `ms-windows-store://pdp/?ProductId=${links.storeId}` : `https://www.microsoft.com/en-us/p/files/${links.storeId}`}
+                target="_blank"
+                rel="noreferrer noopener"
+                style="accent"
+            >
+                {@html ArrowDownload}
+                <div class="hero-button-inner">
+                    <h5>Download {version || ""}</h5>
+                    <span>Get it from Microsoft</span>
+                </div>
+            </Button>
+            <Button
+                href="https://github.com/{links.github.owner}/{links.github.repo}/"
+                target="_blank"
+                rel="noreferrer noopener"
+            >
+                {@html Code}
+                <div class="hero-button-inner">
+                    <h5>View GitHub</h5>
+                    <span>Files is open source!</span>
+                </div>
+            </Button>
+        </div>
     </div>
+    <div class="hero-right">
+        <picture>
+            <source
+                media="(prefers-color-scheme: dark)" 
+                srcset="static/screenshots/hero-dark.png"
+            >
+            <source
+                media="(prefers-color-scheme: light)"
+                srcset="static/screenshots/hero-light.png"
+            >
+            <img
+                class="hero-screenshot"
+                src="static/screenshots/hero-light.png"
+                alt="Files new tab screenshot"
+            >
+        </picture>
+    </div>
+    <hr class="divider"/>
 </PageSection>
 
 <PageSection id="design-section">
@@ -106,18 +126,18 @@
 
     :global {
         #hero-section {
-            @include flex($direction: column, $justify: center);
+            @include flex($direction: row);
+            gap: 4rem;
             overflow: hidden;
-            height: 75vh;
-            min-height: 572px;
-            max-height: 1024px;
+            min-height: 85%;
+            justify-content: space-between;
             canvas {
                 position: absolute;
                 bottom: 0;
                 right: 0;
                 width: 100vh;
                 height: 100vh;
-                opacity: 0.5;
+                opacity: 0.8;
                 mask: radial-gradient(at bottom right, var(--background-base), transparent 70%);
                 z-index: -1;
                 pointer-events: none;
@@ -125,10 +145,63 @@
             .button svg {
                 @include icon($size: 18px);
             }
+
+            .hero-left {
+                @include flex($direction: column, $justify: center);
+                max-width: 40%;
+                height: auto;
+            }
+
+            .hero-right {
+                max-width: 60%;
+                
+            
+                .hero-screenshot {
+                    width: auto;
+                    height: 100%;
+                    background-color: var(--control-color-disabled);
+                    border: 1px solid var(--SurfaceStrokeColorDefault);
+                    border-radius: 7px;
+                    box-sizing: border-box;
+                    backdrop-filter: blur(200px) saturate(150%);
+                    -webkit-backdrop-filter: blur(200px) saturate(150%);
+                    box-shadow: var(--window-elevation);
+                }
+            }
+
+            .divider {
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                right: 0;
+                margin: 0;
+                height: 1px;
+                background-color: var(--DividerStrokeColorDefault);
+                border: none;
+            }
         }
         @media (prefers-color-scheme: dark) {
             #hero-section canvas {
-                opacity: 0.15;
+                opacity: 0.4;
+            }
+        }
+
+        @media only screen and (max-width: 768px) {
+            #hero-section {
+                @include flex($direction: column);
+                min-height: 115vw;
+
+                .hero-left {
+                    max-width: 100%;
+                }
+
+                .hero-right {
+                    max-width: 100%;
+                    .hero-screenshot {
+                        width: 100%;
+                        height: auto;
+                    }
+                }
             }
         }
     }
