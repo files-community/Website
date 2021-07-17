c15ff2c7faff6c10f5252b7a1d097fc9a5de6ff1
Tropical <42101043+Tropix126@users.noreply.github.com>
2021-07-16 21:34:23 -0400

    add some base colors to work with

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -156,7 +156,7 @@
     </div>
 </PageSection>
 
-<PageSection>
+<PageSection id="features-section">
     <HeaderChip>Features</HeaderChip>
 </PageSection>
 
@@ -166,12 +166,12 @@
         <h2>Customize to your liking.</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
         <div class="theme-chooser">
-            <ColorSwatch value={1} bind:group={themes} />
-            <ColorSwatch value={2} bind:group={themes} />
-            <ColorSwatch value={3} bind:group={themes} />
-            <ColorSwatch value={4} bind:group={themes} />
-            <ColorSwatch value={5} bind:group={themes} />
-            <ColorSwatch value={6} bind:group={themes} />
+            <ColorSwatch color="var(--background-tertiary);" value={1} bind:group={themes} />
+            <ColorSwatch color="#3ecc9c" value={2} bind:group={themes} />
+            <ColorSwatch color="#ea5b7a" value={3} bind:group={themes} />
+            <ColorSwatch color="#c34735" value={4} bind:group={themes} />
+            <ColorSwatch color="#274370" value={5} bind:group={themes} />
+            <ColorSwatch color="#88c0d0" value={6} bind:group={themes} />
         </div>
         <div class="buttons-spacer">
             <Button style="accent" href="themes">
