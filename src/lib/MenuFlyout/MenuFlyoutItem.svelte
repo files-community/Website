34007ecfac2bd266346224333643f9c8e2b46fb2
Tropical <42101043+Tropix126@users.noreply.github.com>
2021-07-26 15:25:35 -0400

    remove unused MenuFlyout code

--- src/lib/MenuFlyout/MenuFlyoutItem.svelte
+++ src/lib/MenuFlyout/MenuFlyoutItem.svelte
@@ -1,19 +1,10 @@
 <script lang="ts">
-    import { createEventDispatcher } from "svelte";
-
     export let group = [];
     export let value = undefined;
     export let selected: boolean = false;
     export let disabled: boolean = false;
     export let compact: boolean = false;
     export let type: "default" | "checkbox" | "radio" | "combobox" | "link" = "default";
-
-    const dispatch = createEventDispatcher();
-    
-    function closeMenu() {
-        dispatch("menuclose");
-    }
-
 </script>
 
 <style lang="scss" src="./MenuFlyoutItem.scss"></style>
@@ -35,7 +26,7 @@
         class:selected
         {...$$restProps}
     >
-        <input on:change={closeMenu} bind:checked={selected} bind:group {value} type="checkbox" hidden />
+        <input bind:checked={selected} bind:group {value} type="checkbox" hidden />
         <slot/>
     </label>
     {:else if type === "radio" || type === "combobox"}
@@ -46,7 +37,7 @@
         class:selected={(group === value) || selected}
         {...$$restProps}
     >
-        <input on:change={closeMenu} bind:group {value} checked={selected} type="radio" hidden />
+        <input bind:group {value} checked={selected} type="radio" hidden />
         <slot/>
     </label>
 {/if}
