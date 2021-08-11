95c7af06034a329c947494b4ae679bf973aea0f7
Tropical <42101043+Tropix126@users.noreply.github.com>
2021-08-10 20:20:47 -0500

    remove MenuFlyoutItem

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,7 +5,6 @@ export { default as PageSection } from "./PageSection/PageSection.svelte";
 export { default as HyperlinkButton } from "./HyperlinkButton/HyperlinkButton.svelte";
 export { default as MenuFlyout } from "./MenuFlyout/MenuFlyoutWrapper.svelte";
 export { default as ListViewItem } from "./ListViewItem/ListViewItem.svelte";
-export { default as MenuFlyoutItem } from "./MenuFlyout/MenuFlyoutItem.svelte";
 export { default as TreeView } from "./TreeView/TreeView.svelte";
 export { default as HeaderChip } from "./HeaderChip/HeaderChip.svelte";
 export { default as ComponentShowcase } from "./ComponentShowcase/ComponentShowcase.svelte";
