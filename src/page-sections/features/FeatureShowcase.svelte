1514c704aa06270c8da524f8213af73baa9ecf7f
Tropical <42101043+Tropix126@users.noreply.github.com>
2021-09-04 17:46:17 -0500

    finish themes page

--- src/page-sections/features/FeatureShowcase.svelte
+++ src/page-sections/features/FeatureShowcase.svelte
@@ -51,7 +51,7 @@
         </table>
     </div>
     {:else if feature === 1}    
-    <svg class="backdrop-icon" width="48" height="48" style="--icon-scale: 0.9" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
+    <svg class="backdrop-icon" width="48" height="48" style="--icon-scale: 0.9; --icon-stroke-width: 0.3" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
         <path in:draw={{ duration: 1250 }} d="M24.833 12.5H40.25C42.2543 12.5 43.8913 14.0724 43.9948 16.0508L44 16.25V35.25C44 37.2543 42.4276 38.8913 40.4492 38.9948L40.25 39H7.75C5.74574 39 4.10873 37.4276 4.0052 35.4492L4 35.25V18.999L17.8036 19L18.0297 18.9932C19.0049 18.9344 19.9192 18.4968 20.5769 17.7743L20.724 17.6025L24.833 12.5ZM17.0607 9C17.8933 9 18.7 9.27703 19.3552 9.78393L19.5301 9.92784L21.974 12.066L18.7771 16.0342L18.6826 16.1388C18.4832 16.336 18.223 16.4605 17.9443 16.4921L17.8036 16.5L4 16.499V12.75C4 10.7457 5.57236 9.10873 7.55084 9.0052L7.75 9H17.0607Z" xmlns="http://www.w3.org/2000/svg"/>
     </svg>
     <div class="previewer-showcase">
@@ -151,7 +151,7 @@
         </div>
     </div>
     {:else if feature === 3}
-    <svg class="backdrop-icon" height="20" style="--icon-scale: 0.9; --icon-stroke-width: 0.1" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg">
+    <svg class="backdrop-icon" height="20" style="--icon-scale: 0.9; --icon-stroke-width: 0.125" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg">
         <path in:draw={{ duration: 1200 }} d="M5.49707 3C4.11636 3 2.99707 4.11929 2.99707 5.5V14.5C2.99707 15.8807 4.11636 17 5.49707 17H14.4971C15.8778 17 16.9971 15.8807 16.9971 14.5V7H9.49707C8.66864 7 7.99707 6.32843 7.99707 5.5V3H5.49707Z M8.99707 3V5.5C8.99707 5.77614 9.22093 6 9.49707 6H16.9971V5.5C16.9971 4.11929 15.8778 3 14.4971 3H8.99707Z" />
     </svg>
     <div class="tabs-showcase">
