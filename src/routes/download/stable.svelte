1c7809f0d18b7056361c176728c169207a76dd51
Yair Aichenbaum <39923744+yaichenbaum@users.noreply.github.com>
2022-01-30 19:58:29 -0500

    Update stable.svelte

--- src/routes/download/stable.svelte
+++ src/routes/download/stable.svelte
@@ -6,6 +6,6 @@
 	<Metadata title="Files • Download Stable" image="download" />
 	<meta
 		http-equiv="refresh"
-		content="0; url=ms-appinstaller:?source=https://filescommunity.blob.core.windows.net/files/stable/Files.Package.appinstaller"
+		content="https://filescommunity.blob.core.windows.net/files/stable/Files.Package.appinstaller"
 	>
 </svelte:head>
