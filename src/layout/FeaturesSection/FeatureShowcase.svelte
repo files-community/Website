bcd2efbfedf9962799494714cbc6f3693c36263c
TheOnlyTails <theonlytails@theonlytails.com>
2022-02-07 18:02:59 +0200

    Revert unrelated changes (will be re-introduced in separate PRs).

--- src/layout/FeaturesSection/FeatureShowcase.svelte
+++ src/layout/FeaturesSection/FeatureShowcase.svelte
@@ -3,7 +3,6 @@
 	import PreviewShowcase from "./preview/PreviewShowcase.svelte";
 	import TagsShowcase from "./tags/TagsShowcase.svelte";
 	import TabsShowcase from "./tabs/TabsShowcase.svelte";
-	import "./FeatureShowcase.scss";
 
 	export let feature = 0;
 </script>
@@ -17,3 +16,7 @@
 {:else if feature === 3}
 	<TabsShowcase />
 {/if}
+
+<style global lang="scss">
+	@use "./FeatureShowcase";
+</style>
