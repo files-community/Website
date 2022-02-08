39953b57b1e12a9988d896c66614fbaa746136e5
TheOnlyTails <theonlytails@theonlytails.com>
2022-02-08 21:26:42 +0200

    Move global styles for the feature showcase to a JS import (#146)
    * Import the global styles for the features section in JS. Updated deps.
    
    * Removed accidental import in docs layout.
    
    Co-authored-by: Marcel Wagner <marcel.alex.wagner@outlook.com>

--- src/layout/FeaturesSection/FeatureShowcase.svelte
+++ src/layout/FeaturesSection/FeatureShowcase.svelte
@@ -3,6 +3,7 @@
 	import PreviewShowcase from "./preview/PreviewShowcase.svelte";
 	import TagsShowcase from "./tags/TagsShowcase.svelte";
 	import TabsShowcase from "./tabs/TabsShowcase.svelte";
+	import "./FeatureShowcase.scss";
 
 	export let feature = 0;
 </script>
@@ -16,7 +17,3 @@
 {:else if feature === 3}
 	<TabsShowcase />
 {/if}
-
-<style global lang="scss">
-	@use "./FeatureShowcase";
-</style>
