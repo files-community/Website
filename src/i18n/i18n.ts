5b775fad5e93dd9853167f280d61d4d92cc66bb4
cinqmilleans <cinq.mille.ans@gmail.com>
2022-05-24 17:57:32 +0200

    prefix

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,10 +1,24 @@
 import { i18nObject } from 'typesafe-i18n'
-import { detectLocale, queryStringDetector } from 'typesafe-i18n/detectors';
 import { loadLocale } from './i18n-util.sync';
-import { baseLocale, loadedLocales, locales } from './i18n-util';
+import { baseLocale, locales, loadedLocales } from './i18n-util';
 
-const selectedLocale = detectLocale(baseLocale, locales, queryStringDetector); 
+const getLocale = () =>
+{
+    if (typeof window !== 'undefined')
+    {
+        const hostname = window.location.hostname;
+        for (let locale of locales)
+        {
+            if (hostname.startsWith(locale + '.'))
+            {
+                return locale;
+            }
+        }
+    }
+    return baseLocale;
+};
 
+const selectedLocale = getLocale();
 loadLocale(selectedLocale);
 
 export const text = i18nObject(selectedLocale, loadedLocales[selectedLocale], {});
