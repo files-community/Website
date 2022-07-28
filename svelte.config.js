1569d90d65d6ace1e615668ec1f2fa5a76f84130
TheOnlyTails <theonlytails@theonlytails.com>
2022-07-28 21:34:24 +0300

    enabled prerendering for all pages (#240)
    * enabled prerendering for all pages
    
    * fix(footer): added a link to crowdin and fixed the dead link
    
    Signed-off-by: TheOnlyTails <theonlytails@theonlytails.com>

--- svelte.config.js
+++ svelte.config.js
@@ -17,6 +17,9 @@ const config = {
 			$data: "src/data",
 			$layout: "src/layout"
 		},
+                prerender: {
+                        default: true
+                },
 		adapter: adapter({ edge: true })
 	},
 	preprocess: [
