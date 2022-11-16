594af11d42afac87fdb833cb9b880d066a235f74
theonlytails <theonlytails@theonlytails.com>
2022-11-16 11:59:00 +0200

    added the docs tree to global `PageData` type

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,14 @@
 /// <reference types="@sveltejs/kit" />
+
+declare namespace App {
+	// interface Locals {}
+	interface PageData {
+		docs?: import("$data/docs").DocsTree
+	}
+	// interface Error {}
+	// interface Platform {}
+}
+
 // gives us highlighting on ?raw imports
 declare module "*?raw" {
 	const src: string
