0327b100287d01239f6911b4fb12921ada9addbc
theonlytails <theonlytails@theonlytails.com>
2022-12-15 09:03:58 +0200

    fixed the RSS Format and added new tags

--- src/routes/blog/feed.rss/+server.ts
+++ src/routes/blog/feed.rss/+server.ts
@@ -18,11 +18,21 @@ export const GET: RequestHandler = async ({ url }) => {
 	return new Response(`<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
-<atom:link href="https://${url.hostname}/blog/rss" rel="self" type="application/rss+xml" />
+<atom:link href="https://${
+		url.hostname
+	}/blog/rss" rel="self" type="application/rss+xml" />
 <title>Files News</title>
 <link>https://${url.hostname}/blog</link>
 <description>News about Files, the modern file explorer for Windows</description>
-<channel>
+<image>
+<url>
+https://${url.hostname}/branding/logo-light.svg
+</url>
+<title>Files News</title>
+<link>
+https://${url.hostname}/blog
+</link>
+</image>
 ${posts
 	.map(
 		post => `<item>
@@ -31,6 +41,13 @@ ${posts
 <link>https://${url.hostname}/blog/posts/${post.slug}</link>
 <description>${post.metadata.description}</description>
 <pubDate>${new Date(post.metadata.date).toUTCString()}</pubDate>
+<dc:creator>${post.metadata.author}</dc:creator>
+<enclosure url="https://${url.hostname}${
+			post.metadata.thumbnail
+		}	" length="0" type="image/jpeg"/>
+<media:content medium="image" url="https://${url.hostname}${
+			post.metadata.thumbnail
+		}"/>
 </item>`
 	)
 	.join("")}
