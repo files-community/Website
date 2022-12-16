bcfb24de7a7058dbf59afaf66119aa757a6d0923
theonlytails <theonlytails@theonlytails.com>
2022-12-16 20:09:52 +0200

    fixed RSS feed validity problems

--- src/routes/blog/feed.rss/+server.ts
+++ src/routes/blog/feed.rss/+server.ts
@@ -1,7 +1,7 @@
 import type { Post } from "../+page.server";
 import type { RequestHandler } from "./$types";
 
-export const GET: RequestHandler = async ({ url }) => {
+export const GET: RequestHandler = async ({ url, setHeaders }) => {
 	const modules = import.meta.glob<Post["metadata"]>("../posts/*/+page.md", {
 		eager: true,
 		import: "metadata"
@@ -15,12 +15,16 @@ export const GET: RequestHandler = async ({ url }) => {
 			} as Post)
 	);
 
+	setHeaders({
+		"Content-Type": "application/rss+xml"
+	});
+
 	return new Response(`<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
 <atom:link href="https://${
 		url.host
-	}/blog/rss" rel="self" type="application/rss+xml" />
+	}/blog/feed.rss" rel="self" type="application/rss+xml" />
 <title>Files News</title>
 <link>https://${url.host}/blog</link>
 <description>News about Files, the modern file explorer for Windows</description>
@@ -32,12 +36,11 @@ export const GET: RequestHandler = async ({ url }) => {
 ${posts
 	.map(
 		post => `<item>
-<guid>${post.slug}</guid>
+<guid>https://${url.host}/blog/posts/${post.slug}</guid>
 <title>${post.metadata.title}</title>
 <link>https://${url.host}/blog/posts/${post.slug}</link>
 <description>${post.metadata.description}</description>
 <pubDate>${new Date(post.metadata.date).toUTCString()}</pubDate>
-<dc:creator>${post.metadata.author}</dc:creator>
 <enclosure url="https://${url.host}${
 			post.metadata.thumbnail
 		}" length="0" type="image/jpeg"/>
