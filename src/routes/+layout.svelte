10988e289547ea6bf70911ec04b3b4a360fb5e39
Aleksander Lukasiewicz <aleksander_lukasiewicz@outlook.com>
2023-01-24 22:19:33 +0100

    Added download link into navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,6 +13,7 @@
 	import Home from "@fluentui/svg-icons/icons/home_24_regular.svg?raw";
 	import Book from "@fluentui/svg-icons/icons/book_24_regular.svg?raw";
 	import News from "@fluentui/svg-icons/icons/news_24_regular.svg?raw";
+	import Arrow_Download from "@fluentui/svg-icons/icons/arrow_download_24_regular.svg?raw";
 	// import PaintBrush from "@fluentui/svg-icons/icons/paint_brush_24_regular.svg?raw";
 
 	const { github, discord } = links;
@@ -38,6 +39,11 @@
 			name: $_("navbar.news"),
 			path: "/blog",
 			icon: News
+		},
+		{
+			name: $_("home.design.download"),
+			path: "/download",
+			icon: Arrow_Download
 		}
 	];
 
