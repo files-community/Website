ef3887de23c94873fee281bc3b003a2fa4b7a998
theonlytails <theonlytails@theonlytails.com>
2023-02-05 10:35:12 +0200

    switched to an outline on the feature cards removed unused imports added functions for typing object keys/values/entries added recommended extensions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,3 +10,9 @@ export const externalLink = {
 	target: "_blank",
 	rel: "noreferrer noopener"
 };
+
+type Entries<T, K extends keyof T = keyof T> = Array<[K, T[K]]>
+
+export const keys = <T extends object>(obj: T) => Object.keys(obj) as (keyof T)[];
+export const values = <T extends object>(obj: T) => Object.values(obj) as (T[keyof T])[];
+export const entries = <T extends object>(obj: T) => Object.entries(obj) as Entries<T>;
