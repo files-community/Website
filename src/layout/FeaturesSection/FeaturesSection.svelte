f0621b4f189a2ec6f3d783644db9a1dc12ab6606
yaira2 <39923744+yaira2@users.noreply.github.com>
2023-02-05 16:31:20 -0500

    Feature: Expanded list in features section (#353)

--- src/layout/FeaturesSection/FeaturesSection.svelte
+++ src/layout/FeaturesSection/FeaturesSection.svelte
@@ -3,7 +3,7 @@
 	import { dev } from "$app/environment";
 	import { _ } from "svelte-i18n";
 
-	import { FeatureCard, HeaderChip, PageSection } from "$lib";
+	import { entries, FeatureCard, HeaderChip, PageSection } from "$lib";
 	import type { FeatureCardData } from "$data/features";
 
 	import FeatureShowcase from "./FeatureShowcase.svelte";
@@ -11,63 +11,62 @@
 	import TabDesktop from "@fluentui/svg-icons/icons/tab_desktop_20_regular.svg?raw";
 	import EyeVisible from "@fluentui/svg-icons/icons/eye_20_regular.svg?raw";
 	import Tag from "@fluentui/svg-icons/icons/tag_24_regular.svg?raw";
-
-	let currentFeature = 0;
-
+	import Columns from "@fluentui/svg-icons/icons/panel_left_28_regular.svg?raw";
+	import Archive from "@fluentui/svg-icons/icons/folder_zip_24_regular.svg?raw";
+	
 	let cardPaginationInterval = 16;
 
-	export const featureCards = [
-		{
+	export const featureCards = {
+		tabs: {
+			title: $_("home.features.tabs.title"),
+			description: $_("home.features.tabs.description"),
+			icon: TabDesktop
+		},
+		columns: {
+			title: $_("home.features.columns.title"),
+			description: $_("home.features.columns.description"),
+			icon: Columns
+		},
+		archives: {
+			title: $_("home.features.archives.title"),
+			description: $_("home.features.archives.description"),
+			icon: Archive
+		},
+		cloud: {
 			title: $_("home.features.cloud.title"),
 			description: $_("home.features.cloud.description"),
 			icon: Cloud
 		},
-		{
+		preview: {
 			title: $_("home.features.preview.title"),
 			description: $_("home.features.preview.description"),
 			icon: EyeVisible
 		},
-		{
+		tags: {
 			title: $_("home.features.tags.title"),
 			description: $_("home.features.tags.description"),
 			icon: Tag
-		},
-		{
-			title: $_("home.features.tabs.title"),
-			description: $_("home.features.tabs.description"),
-			icon: TabDesktop
 		}
-	] as const satisfies readonly FeatureCardData[];
+	} as const satisfies { [name: string]: FeatureCardData };
 
-	onMount(() => {
-		if (!dev) {
-			// Iterate through feature cards every n seconds
-			setInterval(() => {
-				currentFeature += currentFeature !== 3 ? 1 : -currentFeature;
-				cardPaginationInterval = 16;
-			}, cardPaginationInterval * 1000);
-		}
-	});
+	let currentFeature: keyof typeof featureCards = "tabs";
 </script>
 
 <PageSection id="features-section">
-	<div class="features-section-left">
-		<FeatureShowcase feature={currentFeature} />
-	</div>
 	<div class="features-section-right">
 		<HeaderChip>{$_("home.features.chip")}</HeaderChip>
 		<h2>{$_("home.features.title")}</h2>
 		<p>{$_("home.features.description")}</p>
 		<hr />
 		<div class="feature-cards-container">
-			{#each featureCards as feature, i}
+			{#each entries(featureCards) as [id, feature]}
 				<FeatureCard
 					on:click={() => {
-						currentFeature = i;
+						currentFeature = id;
 						cardPaginationInterval = 24;
 					}}
 					clickable
-					selected={currentFeature === i}
+					selected={currentFeature === id}
 					description={feature.description}
 					icon={feature.icon}
 				>
