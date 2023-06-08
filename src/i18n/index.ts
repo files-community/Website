f261e25fe3afed16dae5e0246895eb9ad6691b0e
theonlytails <theonlytails@theonlytails.com>
2023-06-08 21:01:52 +0300

    statically build the locales object from the locale files

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,9 +1,7 @@
 import { addMessages, getLocaleFromNavigator, init } from "svelte-i18n";
-import en from "./locales/en.json";
-import heIL from "./locales/he-IL.json";
 
 type LocaleDictionary = typeof addMessages extends (
-	a: any,
+	locale: string,
 	locales: infer T,
 	...other: any
 ) => any
@@ -15,16 +13,20 @@ type Locale = {
 	readonly dir: "ltr" | "rtl";
 };
 
-export const locales = {
-	en: {
-		values: en,
-		dir: "ltr",
-	},
-	"he-IL": {
-		values: heIL,
-		dir: "rtl",
-	},
-} as const satisfies Record<string, Locale>;
+export const locales: Record<string, Locale> = Object.fromEntries(
+	Object.entries(
+		import.meta.glob<LocaleDictionary>("./locales/*.json", {
+			import: "default",
+			eager: true,
+		})
+	).map(([locale, values]) => [
+		locale.match(/\.\/locales\/([\w-]+)\.json/)?.[1],
+		{
+			values,
+			dir: ["he-IL", "ar-SA"].includes(locale) ? "rtl" : "ltr",
+		},
+	])
+);
 
 export default () => {
 	for (const [locale, localeObject] of Object.entries(locales)) {
