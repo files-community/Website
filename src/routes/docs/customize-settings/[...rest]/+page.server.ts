ba359f5d83372ac0cae8a4ae50671105fd5a7279
Yair <39923744+yaira2@users.noreply.github.com>
2024-01-31 07:31:15 -0800

    Update src/routes/docs/customize-settings/[...rest]/+page.server.ts
    Co-authored-by: TheOnlyTails <theonlytails@theonlytails.com>

--- src/routes/docs/customize-settings/[...rest]/+page.server.ts
+++ src/routes/docs/customize-settings/[...rest]/+page.server.ts
@@ -4,5 +4,5 @@ import { redirect } from "@sveltejs/kit";
 export const load = ({ params }) => {
   if (params.rest === "custom-themes") redirect("/docs/customize-settings/appearance");
   if (params.rest === "replace-file-explorer") redirect("/docs/customize-settings/advanced");
-  redirect(`/docs/customize-settings/${params.rest}`)
+  return
 };
