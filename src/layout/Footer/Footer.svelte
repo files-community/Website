e6746c691d88683073020c895c25c5279d50d4f6
Lamparter <71598437+Lamparter@users.noreply.github.com>
2024-12-05 20:32:20 +0000

    Added ability to display minimal UI with `?minimal` (#737)

--- src/layout/Footer/Footer.svelte
+++ src/layout/Footer/Footer.svelte
@@ -3,6 +3,7 @@
 	import { Button, IconButton } from "fluent-svelte";
 	import { links } from "$data/links";
 	import { _ } from "svelte-i18n";
+	import { onMount } from "svelte";
 	import Github from "./icons/github.svg?raw";
 	import Discord from "./icons/discord.svg?raw";
 
@@ -24,98 +25,107 @@
 			link: "https://michaelwe.st/",
 		},
 	];
+
+	let minimal = false;
+
+	onMount(() => {
+		const params = new URLSearchParams(window.location.search);
+		minimal = params.has("minimal");
+	});
 </script>
 
-<PageSection type="footer" id="page-footer">
-	<div class="column">
-		<a class="logo" href="/">
-			<picture>
-				<source
-					media="(prefers-color-scheme: dark)"
-					srcset="/branding/logo-dark.svg"
-				/>
-				<source
-					media="(prefers-color-scheme: light)"
-					srcset="/branding/logo-light.svg"
-				/>
-				<img
-					alt="Files logo"
-					height="32"
-					src="/branding/logo-light.svg"
-					width="32"
-				/>
-			</picture>
-			Files
-		</a>
-		<div class="social-links">
-			<IconButton
-				href="https://github.com/{links.github.owner}/{links.github.repo}/"
-				title="GitHub repository"
-				aria-label="GitHub repository"
-				{...externalLink}
-			>
-				{@html Github}
-			</IconButton>
-			<IconButton
-				href="https://discord.gg/{links.discord}"
-				title="Discord server"
-				aria-label="Discord server"
-				{...externalLink}
-			>
-				{@html Discord}
-			</IconButton>
+{#if !minimal}
+	<PageSection type="footer" id="page-footer">
+		<div class="column">
+			<a class="logo" href="/">
+				<picture>
+					<source
+						media="(prefers-color-scheme: dark)"
+						srcset="/branding/logo-dark.svg"
+					/>
+					<source
+						media="(prefers-color-scheme: light)"
+						srcset="/branding/logo-light.svg"
+					/>
+					<img
+						alt="Files logo"
+						height="32"
+						src="/branding/logo-light.svg"
+						width="32"
+					/>
+				</picture>
+				Files
+			</a>
+			<div class="social-links">
+				<IconButton
+					href="https://github.com/{links.github.owner}/{links.github.repo}/"
+					title="GitHub repository"
+					aria-label="GitHub repository"
+					{...externalLink}
+				>
+					{@html Github}
+				</IconButton>
+				<IconButton
+					href="https://discord.gg/{links.discord}"
+					title="Discord server"
+					aria-label="Discord server"
+					{...externalLink}
+				>
+					{@html Discord}
+				</IconButton>
+			</div>
+			<p>
+				{$_("Copyright (c) {year} Files Community", {
+					values: { year: new Date().getFullYear() },
+				})}
+			</p>
 		</div>
-		<p>
-			{$_("Copyright (c) {year} Files Community", {
-				values: { year: new Date().getFullYear() },
-			})}
-		</p>
-	</div>
-	<div class="column">
-		<p>Pages</p>
-		<Button variant="hyperlink" href="/">
-			{$_("footer.home", defaultI18nValues)}
-		</Button>
-		<Button variant="hyperlink" href="/docs">
-			{$_("footer.docs", defaultI18nValues)}
-		</Button>
-		<Button variant="hyperlink" href="/blog">
-			{$_("footer.news", defaultI18nValues)}
-		</Button>
-		<Button variant="hyperlink" href="/download">
-			{$_("footer.download", defaultI18nValues)}
-		</Button>
-	</div>
-	<div class="column">
-		<p>{$_("footer.contribute", defaultI18nValues)}</p>
-		<Button
-			variant="hyperlink"
-			{...externalLink}
-			href="https://github.com/{links.github.owner}/{links.github
-				.repo}/issues/new/choose"
-		>
-			{$_("footer.feedback", defaultI18nValues)}
-		</Button>
+		<div class="column">
+			<p>Pages</p>
+			<Button variant="hyperlink" href="/">
+				{$_("footer.home", defaultI18nValues)}
+			</Button>
+			<Button variant="hyperlink" href="/docs">
+				{$_("footer.docs", defaultI18nValues)}
+			</Button>
+			<Button variant="hyperlink" href="/blog">
+				{$_("footer.news", defaultI18nValues)}
+			</Button>
+			<Button variant="hyperlink" href="/download">
+				{$_("footer.download", defaultI18nValues)}
+			</Button>
+		</div>
+		<div class="column">
+			<p>{$_("footer.contribute", defaultI18nValues)}</p>
+			<Button
+				variant="hyperlink"
+				{...externalLink}
+				href="https://github.com/{links.github.owner}/{links.github
+					.repo}/issues/new/choose"
+			>
+				{$_("footer.feedback", defaultI18nValues)}
+			</Button>
 
-		<Button variant="hyperlink" href="/docs/contributing/code-style">
-			{$_("footer.style_guide", defaultI18nValues)}
-		</Button>
+			<Button variant="hyperlink" href="/docs/contributing/code-style">
+				{$_("footer.style_guide", defaultI18nValues)}
+			</Button>
 
-		<Button
-			{...externalLink}
-			href="https://crowdin.com/project/files-app"
-			variant="hyperlink"
-		>
-			{$_("footer.translations", defaultI18nValues)}
-		</Button>
-	</div>
-	<div class="column">
-		<p>{$_("footer.support", defaultI18nValues)}</p>
-		<Button variant="hyperlink" href="/privacy">
-			{$_("footer.privacy", defaultI18nValues)}
-		</Button>
-	</div>
-</PageSection>
+			<Button
+				{...externalLink}
+				href="https://crowdin.com/project/files-app"
+				variant="hyperlink"
+			>
+				{$_("footer.translations", defaultI18nValues)}
+			</Button>
+		</div>
+		<div class="column">
+			<p>{$_("footer.support", defaultI18nValues)}</p>
+			<Button variant="hyperlink" href="/privacy">
+				{$_("footer.privacy", defaultI18nValues)}
+			</Button>
+		</div>
+	</PageSection>
+{/if}
 
 <style lang="scss">
 	@use "./Footer";
--- src/routes/blog/posts/+layout.svelte
+++ src/routes/blog/posts/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import { defaultI18nValues, externalLink, Metadata } from "$lib";
+	import { onMount } from "svelte";
 	import { _ } from "svelte-i18n";
 	import Share from "~icons/fluent/share-24-regular";
 	import ArrowLeft from "~icons/fluent/arrow-left-24-regular";
+	import Open from "~icons/fluent/open-24-regular";
 	import { IconButton, MenuFlyout, MenuFlyoutItem } from "fluent-svelte";
 	import { page } from "$app/stores";
 	import type { LayoutData } from "./$types";
@@ -11,13 +13,20 @@
 
 	$: ({ title, thumbnail, author, description, date, slug } = data);
 	$: pageTitle = title;
+
+	let minimal = false;
+
+	onMount(() => {
+		const params = new URLSearchParams(window.location.search);
+		minimal = params.has("minimal");
+	});
 </script>
 
 <Metadata
 	title={pageTitle
 		? $_("metadata.blog_page", {
 				values: { title: pageTitle, ...defaultI18nValues.values },
-		  })
+			})
 		: $_("metadata.blog_home", defaultI18nValues)}
 	image={thumbnail}
 	{description}
@@ -26,15 +35,33 @@
 <section class="blog-post">
 	<article>
 		<div class="post-title">
-			<IconButton
-				--icon-color="var(--text-color-secondary)"
-				aria-label="Back to Blog"
-				class="back-button"
-				href="/blog"
-				title="Back to Blog"
-			>
-				<ArrowLeft />
-			</IconButton>
+			{#if !minimal}
+				<IconButton
+					--icon-color="var(--text-color-secondary)"
+					aria-label="Back to Blog"
+					class="back-button"
+					href="/blog"
+					title="Back to Blog"
+				>
+					<ArrowLeft />
+				</IconButton>
+			{/if}
+			{#if minimal}
+				<IconButton
+					--icon-color="var(--text-color-secondary)"
+					aria-label="View full post"
+					class="back-button"
+					href={window.location.href.replace("?minimal", "")}
+					title="View full post"
+					on:click={() =>
+						(window.location.href = window.location.href.replace(
+							"?minimal",
+							"",
+						))}
+				>
+					<Open />
+				</IconButton>
+			{/if}
 			<h1 style:view-transition-name="post-title-{slug}">{title}</h1>
 		</div>
 		<div class="post-info">
@@ -51,57 +78,59 @@
 					month: "short",
 				},
 			)}
-			<MenuFlyout placement="bottom">
-				<IconButton
-					size={20}
-					aria-label="Share"
-					class="share-button"
-					title="Share"
-				>
-					<Share />
-				</IconButton>
-				<svelte:fragment slot="flyout">
-					<MenuFlyoutItem
-						on:click={() =>
-							navigator.share({
-								title,
-								url: $page.url.href,
-							})}
+			{#if !minimal}
+				<MenuFlyout placement="bottom">
+					<IconButton
+						size={20}
+						aria-label="Share"
+						class="share-button"
+						title="Share"
 					>
-						{$_("blog.share.default", defaultI18nValues)}
-					</MenuFlyoutItem>
-					<MenuFlyoutItem
-						on:click={() => navigator.clipboard.writeText($page.url.href)}
-					>
-						{$_("blog.share.url", defaultI18nValues)}
-					</MenuFlyoutItem>
-					<MenuFlyoutItem>
-						<a
-							href="https://twitter.com/intent/tweet?text={encodeURIComponent(
-								$page.url.href,
-							)}"
-							{...externalLink}
+						<Share />
+					</IconButton>
+					<svelte:fragment slot="flyout">
+						<MenuFlyoutItem
+							on:click={() =>
+								navigator.share({
+									title,
+									url: $page.url.href,
+								})}
 						>
-							{$_("blog.share.tweet", defaultI18nValues)}
-						</a>
-					</MenuFlyoutItem>
-					<MenuFlyoutItem>
-						<a
-							href="https://www.facebook.com/sharer/sharer.php?u={encodeURIComponent(
-								$page.url.href,
-							)}"
-							{...externalLink}
+							{$_("blog.share.default", defaultI18nValues)}
+						</MenuFlyoutItem>
+						<MenuFlyoutItem
+							on:click={() => navigator.clipboard.writeText($page.url.href)}
 						>
-							{$_("blog.share.facebook", defaultI18nValues)}
-						</a>
-					</MenuFlyoutItem>
-					<MenuFlyoutItem>
-						<a href="/blog/feed.rss" {...externalLink}
-							>{$_("blog.share.rss", defaultI18nValues)}</a
-						>
-					</MenuFlyoutItem>
-				</svelte:fragment>
-			</MenuFlyout>
+							{$_("blog.share.url", defaultI18nValues)}
+						</MenuFlyoutItem>
+						<MenuFlyoutItem>
+							<a
+								href="https://twitter.com/intent/tweet?text={encodeURIComponent(
+									$page.url.href,
+								)}"
+								{...externalLink}
+							>
+								{$_("blog.share.tweet", defaultI18nValues)}
+							</a>
+						</MenuFlyoutItem>
+						<MenuFlyoutItem>
+							<a
+								href="https://www.facebook.com/sharer/sharer.php?u={encodeURIComponent(
+									$page.url.href,
+								)}"
+								{...externalLink}
+							>
+								{$_("blog.share.facebook", defaultI18nValues)}
+							</a>
+						</MenuFlyoutItem>
+						<MenuFlyoutItem>
+							<a href="/blog/feed.rss" {...externalLink}
+								>{$_("blog.share.rss", defaultI18nValues)}</a
+							>
+						</MenuFlyoutItem>
+					</svelte:fragment>
+				</MenuFlyout>
+			{/if}
 		</div>
 		{#if thumbnail}
 			<img
