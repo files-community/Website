388ee6615072a1d105684d06dff38fc466e1806f
Lamparter <71598437+Lamparter@users.noreply.github.com>
2025-01-06 07:51:20 +0000

    Delete `src/data/minimalism.ts`

--- src/data/minimalism.ts
+++
@@ -1,8 +0,0 @@
-import { onMount } from "svelte";
-
-export let minimal = false;
-
-onMount(() => {
-	const params = new URLSearchParams(window.location.search);
-	minimal = params.has("minimal");
-});
